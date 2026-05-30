94b2585d18764ebccdb822c0a2b224974065e650
Sergey <s.prihodchenko@yandex.ru>
2026-05-30 21:56:19 +0300

    a

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -16,13 +16,12 @@ class TelegramController extends Controller
             $user = TgUser::where('telegram_id', $data['telegram_id'])->first();
         } else {
             $user = TgUser::create($data);
-        }
-
-        try {
-            dispatch(new \App\Jobs\UploadAvatarTgUser($data['telegram_id']));
-        } catch (\Exception $e) {
-            Log::error("Failed to dispatch UploadAvatarTgUser job: " . $e->getMessage());
-            \App\Jobs\UploadAvatarTgUser::dispatchSync($data['telegram_id']);
+            try {
+                dispatch(new \App\Jobs\UploadAvatarTgUser($data['telegram_id']));
+            } catch (\Exception $e) {
+                Log::error("Failed to dispatch UploadAvatarTgUser job: " . $e->getMessage());
+                \App\Jobs\UploadAvatarTgUser::dispatchSync($data['telegram_id']);
+            }
         }
 
         return response()->json(['user' => $user->toArray()]);
